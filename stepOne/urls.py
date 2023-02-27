2daeb86566c6dfd03e5e7fe840b1d1844a9425ae
Mahmoud Adel <110413664+mahmoud6565dsv@users.noreply.github.com>
2023-02-27 14:42:31 +0200

    Update urls.py

--- stepOne/urls.py
+++ stepOne/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('small_app.urls', namespace='calcs')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 
